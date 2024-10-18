4dd769cdbc176b46e4c9bba20fc5f954aa4af185
MoLala1 <Mohammed.lala1@hmcts.net>
2024-10-18 12:21:21 +0100

    config changes

--- src/main/server.ts
+++ src/main/server.ts
@@ -15,7 +15,7 @@ let httpsServer: https.Server | null = null;
 app.locals.shutdown = false;
 
 // TODO: set the right port for your application
-const port: number = parseInt(process.env.PORT || '3100', 10);
+const port: number = parseInt(process.env.PORT || '3101', 10);
 
 if (app.locals.ENV === 'development') {
   const sslDirectory = path.join(__dirname, 'resources', 'localhost-ssl');
